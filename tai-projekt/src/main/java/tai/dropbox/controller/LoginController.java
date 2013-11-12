1605f6c66e98e08f97f39f52b9930254a045a1db
majcherek <ma.majcherek@gmail.com>
2013-11-12 20:32:39 +0100

    login controller, unauthorized view

--- tai-projekt/src/main/java/tai/dropbox/controller/LoginController.java
+++ tai-projekt/src/main/java/tai/dropbox/controller/LoginController.java
@@ -1,12 +1,19 @@
 package tai.dropbox.controller;
 
-import java.util.List;
+import java.util.Locale;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dropbox.core.DbxAppInfo;
+import com.dropbox.core.DbxClient;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.DbxWebAuthNoRedirect;
 
 
 @Controller
@@ -15,26 +22,44 @@ public class LoginController {
 	@Autowired
 	private JdbcTemplate template;
 	
+	private static final String APP_KEY = "xwaq9ujw7aoskjm";
+	private static final String APP_SECRET = "2znk2stxkigwknv";
+	
+	private DbxAppInfo dbxAppInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+	private DbxRequestConfig dbxRequestConfig = new DbxRequestConfig("tai test", Locale.getDefault().toString());
+	
 	@RequestMapping("/index.htm")
-	public ModelAndView accountSummary() {
-//		List<Account> accounts = accountManager.getAllAccounts();
-//		ModelAndView mav = new ModelAndView();
-//		mav.setViewName("/WEB-INF/views/accountSummary.jsp");
-//		mav.addObject("accounts", accounts);
-//		return mav;
+	public ModelAndView getIndexPage() {
 		
-		String token = template.queryForObject("select access_token from users where username = ?", new Object[]{"admin"}, String.class);
+		Subject user = SecurityUtils.getSubject();		
+		String accessToken = template.queryForObject("select access_token from users where username = ?", new Object[]{user.getPrincipal()}, String.class);
 
-		if(token == null){
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("unauthorized");
-			return mav;
+		if(accessToken == null){
+			return getUnauthorized();
 		}else{
-			ModelAndView mav = new ModelAndView();
-			mav.setViewName("index");
-			return mav;
-		}
+			return getAuthorized(accessToken);
+		}		
+
+	}
+
+	private ModelAndView getAuthorized(String accessToken) {
+		DbxClient client = new DbxClient(dbxRequestConfig, accessToken);
 		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+
+	private ModelAndView getUnauthorized() {
+
+		DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(dbxRequestConfig, dbxAppInfo);
+		String authorizeUrl = webAuth.start();
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("unauthorized");
+		mav.addObject("link", authorizeUrl);
+		return mav;
+
 
 	}
 
--- tai-projekt/src/main/webapp/unauthorized.jsp
+++ tai-projekt/src/main/webapp/unauthorized.jsp
@@ -1,14 +1,7 @@
 <html>
 <body>
-	<center>
-	<h1>LOGIN FORM</h1>
-	<form id="loginform" name="loginform" method="post" action="" accept-charset="utf-8">
-	  <label for="username">Username</label>: <input name="username" id="username" type="text" /><br/><br/>
-	  <label for="password">Password</label>: <input name="password" id="password" type="password" /><br/><hr/>
-	  <input type="submit" name="Login" value="Login" />
-	</form>
-	</center>
-	<script src="res/js/jquery.js" type="text/javascript"></script>
-	<script src="res/js/bootstrap.min.js" type="text/javascript"></script>
+	<label>${link}</label><br/>
+ 	<label>Code:</label> <input name="code" id="code" type="text" />
+	<input type="submit" name="Ok" value="Ok"  />
 </body>
 </html>

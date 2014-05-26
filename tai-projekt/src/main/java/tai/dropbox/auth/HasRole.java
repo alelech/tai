d0ec6dfdefd00d53ef2db168ca1c4641e91bc219
molok <olek.lech@gmail.com>
2014-05-26 11:52:32 +0200

    roles2

---
+++ tai-projekt/src/main/java/tai/dropbox/auth/HasRole.java
@@ -0,0 +1,12 @@
+package tai.dropbox.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HasRole {
+   String role();
+}
